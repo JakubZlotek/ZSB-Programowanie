37888378f0b63f6ab44ca7825643669443c4ea18
Jakub Złotek <kubazlotek@gmail.com>
2020-10-04 21:05:25 +0200

    funkcje python

---
+++ Python/10. Funkcje/main.py
@@ -0,0 +1,11 @@
+def zwierzefnc(zwierze):
+    if zwierze == "Kot":
+        return "Ma dlugi ogon"
+    else if zwierze == "Pies":
+        return "Merda ogonem"
+    else:
+        return "Niestety nie znam takiego zwierzecia :("
+
+print(zwierzefnc("Kot"))
+print(zwierzefnc("Pies"))
+print(zwierzefnc("Lemur Madagaskarski"))
