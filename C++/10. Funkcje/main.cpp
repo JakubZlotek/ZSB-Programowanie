1b615278f85c8b531838a9c283adc11dadbafb37
Jakub Złotek <kubazlotek@gmail.com>
2020-10-04 20:55:37 +0200

    funkcje

---
+++ C++/10. Funkcje/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void ulubiona(int liczba) {
+  cout << "Moja ulubiona liczba to: " << liczba;
+}
+
+string zwierzefnc(string zwierze){
+    if (zwierze == "Kot") {
+      return "Ma dlugi ogon";
+    } else if (zwierze == "Pies") {
+      return "Merda ogonem";
+    } else {
+      return "Nie znam takiego zwierzecia :(";
+    }
+}
+
+int main()
+{
+    liczba(5);
+    cout << zwierzefnc("Kot") << endl;
+    cout << zwierzefnc("Pies") << endl;
+    cout << zwierzefnc("Strus") << endl;
+    return 0;
+}
